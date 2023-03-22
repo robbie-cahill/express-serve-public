4aecd8559742bd1ca86e17710ecba569157d3b06
Robbie Cahill <robbie-cahill@proton.me>
2023-03-22 20:50:10 +1100

    Add examples

---
+++ examples/randomSubdoman.js
@@ -0,0 +1,19 @@
+/**
+ * An example to get an instant randomized public URL for your express app.
+ *
+ * Heavily based on the Hello World example from https://expressjs.com/en/starter/hello-world.html
+ */
+import { servePublic } from "express-serve-public";
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+    response.send("Hello World!");
+});
+
+servePublic(app, {
+    port: 3000,
+}, () => {
+    console.log(`Example app listening on port 3000`);
+});
